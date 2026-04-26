7ddac94266fc61dff50e94cf60251dcdfe8e511a
Juan <duvidobanir12@gmail.com>
2026-04-26 15:21:17 -0300

    Add edit and delete functions to index.html

---
+++ api/checkAppointments.js
@@ -0,0 +1,9 @@
+// Logic to check appointments
+const checkAppointments = function() {
+    const telegramBotToken = "8674468488:AAFFhbmaE3gImV3o7iLieo_MjGaNspjecvA"; // Your Telegram Bot Token
+    const telegramChatId = "5106987392"; // Your Telegram Chat ID
+
+    // Code to check appointments and notify
+};
+
+module.exports = checkAppointments;
